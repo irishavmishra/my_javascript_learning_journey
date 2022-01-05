69a2f261d9bfa315cd6cf56f389b5fdacdea6086
irishavmishra <irishavmishra@proton.me>
2022-01-05 07:39:00 +0530

    My javascript learning journey

---
+++ 10templatestring.js
@@ -0,0 +1,11 @@
+// template String
+let age = 22;
+let firstName = "rishav";
+
+// "my name is rishav and my age is 21"
+// let aboutMe = "my name is " + firstName + " and my age is " + age;
+// console.log(aboutMe);
+
+let aboutMe = `my name is ${firstName} and my age is ${age}`;
+
+console.log(aboutMe);
