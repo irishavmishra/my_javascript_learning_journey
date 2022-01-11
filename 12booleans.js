f3b1d66a75a61550bb7e2bb9bb7309c751fbb3b5
irishavmishra <irishavmishra@proton.me>
2022-01-11 11:39:00 +0530

    My javascript learning journey

---
+++ 12booleans.js
@@ -0,0 +1,24 @@
+// booleans & comparison operator
+
+// Booleans
+// true false
+
+let num1 = "7";
+let num2 = 8;
+let num3 = 7;
+let num4 = 7;
+
+// console.log(num1<num2);
+// console.log(num1>num2);
+// console.log(num1 >= num2);
+// console.log(num1 <= num2);
+
+// == vs ===
+
+// console.log(num1 == num2);
+// console.log(num3 === num4);
+
+// != vs !==
+
+// console.log(num1 != num2);
+console.log(num4 !== num3);
