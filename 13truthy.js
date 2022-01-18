80f2f2448f49f1692d3175c27eaf67f2e5cbd89d
irishavmishra <irishavmishra@proton.me>
2022-01-18 07:39:00 +0530

    My javascript learning journey

---
+++ 13truthy.js
@@ -0,0 +1,25 @@
+// truthy and falsy values
+
+
+// falsy values
+
+
+// false 
+// ""
+// null 
+// undefined
+// 0
+
+
+// truthy 
+"abc"
+1, -1
+
+
+let firstName = "rj";
+
+if(firstName){
+    console.log(firstName);
+}else{
+    console.log("false value")
+}
