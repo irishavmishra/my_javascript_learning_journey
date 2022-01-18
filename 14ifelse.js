8b76dbc0f1b73f472d3295e60effc407c05e9ab2
irishavmishra <irishavmishra@proton.me>
2022-01-18 07:39:00 +0530

    My javascript learning journey

---
+++ 14ifelse.js
@@ -0,0 +1,19 @@
+// // if else condition 
+
+// let age = 16;
+
+// if(age >=18){
+//     console.log("user can play pubg");
+// }else{
+//     console.log("user can play free fire");
+// }
+
+
+// let num = 14;
+// if(num%2===0){
+//     console.log("even")
+// }else{
+//     console.log("odd")
+// };
+
+
