7fa90228ba4f93b2a5275d034a2672d8146d94fb
irishavmishra <irishavmishra@proton.me>
2022-01-19 07:39:00 +0530

    My javascript learning journey

---
+++ 15ternaryoperator.js
@@ -0,0 +1,19 @@
+// ternary operator
+
+
+// let age = 13;
+// let drink;
+
+// if(age>=15){
+//     drink="coffee";
+// }else{
+//     drink="milk";
+// };
+
+// console.log(drink);
+
+// ternary operator / conditional operator 
+
+// let age = 14;
+// let drink = age >=15 ? "coffee" : "milk"
+// console.log(drink);
