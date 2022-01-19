0b4473ce8f68f629e7374269c1668769076dc7fd
irishavmishra <irishavmishra@proton.me>
2022-01-19 07:39:00 +0530

    My javascript learning journey

---
+++ 18ifelseif.js
@@ -0,0 +1,13 @@
+let temp = 30;
+
+if (temp < 20) {
+  console.log("normal");
+} else if (temp < 25) {
+  console.log("hot");
+} else if (temp < 30) {
+  console.log("very hot");
+} else if (temp < 35) {
+  console.log("extremely hot");
+} else {
+  console.log("not survive");
+}
