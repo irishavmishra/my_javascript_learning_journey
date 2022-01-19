5ee1edc410132e8706a92d9645e700f56f4230d7
irishavmishra <irishavmishra@proton.me>
2022-01-19 11:15:00 +0530

    My javascript learning journey

---
+++ 16andoroperator.js
@@ -0,0 +1,25 @@
+// And&& or|| operator
+
+// and &&
+// let firstName = "ritik";
+// let age = 15;
+
+// if (firstName[0] === "r" && age>=18){
+//     console.log("name start with r and age is above 18")
+// }else{
+//     console.log("false")
+// };
+
+// or ||
+
+let firstName="gritik";
+let age=17;
+
+if(firstName[0] === "r" || age>=18 ){
+    console.log("first name start with r and your age is above 18")
+}else{
+    console.log("else")
+}
+
+
+
