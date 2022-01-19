9641e62b42d4dff6c48f96ae0ea09b4150a0cbe4
irishavmishra <irishavmishra@proton.me>
2022-01-19 07:39:00 +0530

    My javascript learning journey

---
+++ 19switchstatement.js
@@ -0,0 +1,31 @@
+// switch statement 
+
+let day=8;
+
+switch(day){
+    case 0:
+        console.log("sunday");
+        break;
+    case 1:
+        console.log("monday");
+        break;
+    case 2:
+        console.log("twesday");
+        break;
+    case 3:
+        console.log("wednesday");
+        break;
+    case 4:
+        console.log("thursday");
+        break;
+    case 5:
+        console.log("friday");
+        break;
+    case 6:
+        console.log("saturday");
+        break;
+    default:
+        console.log("invalid")
+    
+
+};
