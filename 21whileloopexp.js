cfd1c20ff2cace1914a6b9bd4ab308dfd1675208
irishavmishra <irishavmishra@proton.me>
2022-06-09 07:39:00 +0530

    My javascript learning journey

---
+++ 21whileloopexp.js
@@ -0,0 +1,17 @@
+// while loop example
+let num =10000;
+// let total = 0;
+// let i=0;
+
+// while(i<=num){
+//     total=total+i;
+//     i++;
+
+// }
+
+
+
+// console.log(total);
+
+let total = (num*(num+1))/2
+console.log(total)
