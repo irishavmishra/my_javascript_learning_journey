244cd7defffdd1afe0d18851c0fc11572495a736
irishavmishra <irishavmishra@proton.me>
2022-06-09 07:39:00 +0530

    My javascript learning journey

---
+++ 24breakcontinew.js
@@ -0,0 +1,19 @@
+// Break keyword
+
+// continue keyword
+
+for(let i=0; i<=10; i++){
+
+    if(i===4){
+        break
+    }
+    console.log(i)
+}
+
+for(let i=0; i<=10; i++){
+
+    if(i===4){
+        continue;
+    }
+    console.log(i)
+}
