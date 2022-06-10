90ff6c837c3f231aee40c6c88b73b2026dc8a4d3
irishavmishra <irishavmishra@proton.me>
2022-06-10 07:39:00 +0530

    My javascript learning journey

---
+++ 27arrayspushpop.js
@@ -0,0 +1,21 @@
+//array push pop
+
+//array shift unswift
+
+
+let fruits=["apple", "mango" ,"banana"]; 
+// fruits.push("water");
+// fruits.push("pineapple");
+
+// // fruits.pop();
+// let poppedfruit=fruits.pop();
+// console.log("popped fruit is", poppedfruit)
+// console.log(fruits);
+
+// //unshift
+// fruits.unshift("pea");
+// console.log(fruits);
+
+// fruits.shift();
+// console.log(fruits);
+console.log(fruits.shift());
