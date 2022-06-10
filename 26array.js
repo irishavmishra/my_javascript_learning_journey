19de704173cbf08a25995a72b04a11b4bf5bb0fd
irishavmishra <irishavmishra@proton.me>
2022-06-10 07:39:00 +0530

    My javascript learning journey

---
+++ 26array.js
@@ -0,0 +1,31 @@
+// intro to arrays
+// reference types
+// how to create arrays
+
+// ordered colection of items
+
+// let fruits=["apple", "mango" ,"banana"];
+// console.log(fruits);
+// fruits[1]="grapes";
+// console.log(fruits);
+
+
+// let number=[1,2,3,4];
+// console.log(number);
+
+
+// let mix=[1,2,3,4,3.4,5.9, "string", null, undefined];
+// console.log(mix);
+
+let fruits=["apple", "mango" ,"banana"];    //arrays
+console.log(typeof fruits);
+console.log(Array.isArray(fruits));      //to check it is arrray or object
+  
+//becouse in simpal methord they always show object
+
+
+let obj={};    //object
+console.log(typeof obj);
+console.log(Array.isArray(obj));
+
+
