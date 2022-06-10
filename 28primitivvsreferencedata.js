42eba3e61e281062a52d10f561decc31f71c169e
irishavmishra <irishavmishra@proton.me>
2022-06-10 07:39:00 +0530

    My javascript learning journey

---
+++ 28primitivvsreferencedata.js
@@ -0,0 +1,21 @@
+// primitive vs reference data types
+
+// // primitive
+// let num1=5;
+// let num2=num1;
+// console.log(num1);
+// console.log(num2);
+// num1++
+// console.log(num1);
+// console.log(num2);
+
+
+// reference data types
+
+let array1=["apple","ball"];
+let array2=array1;
+console.log(array1);
+console.log(array2);
+array1.push("cat")
+console.log(array1);
+console.log(array2);
