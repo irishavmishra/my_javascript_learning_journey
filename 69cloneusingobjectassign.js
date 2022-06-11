55bea66e39929b31bb3f917684fc4fb063d300e0
irishavmishra <irishavmishra@proton.me>
2022-06-11 07:39:00 +0530

    My javascript learning journey

---
+++ 69cloneusingobjectassign.js
@@ -0,0 +1,14 @@
+// clone using Object.assign 
+// memory
+
+const obj={
+    key1:"value1",
+    key2:"value2"
+}
+
+// const obj2 = obj;
+// const obj2 = {...obj};
+const obj2 = Object.assign({},obj);
+obj.key3="value3"
+console.log(obj);
+console.log(obj2);
