06dd4809d36a9d4f6beae9f2262b35b8ebd16b75
irishavmishra <irishavmishra@proton.me>
2022-06-14 07:39:00 +0530

    My javascript learning journey

---
+++ 36howtocreatobject.js
@@ -0,0 +1,14 @@
+// how to creat object 
+
+const person={
+    name: "Rishav",
+    age: 25,
+    hobbies:[ "exploring","sleeping","traveling"]
+
+
+}
+// person.gender="male"
+person["gender"]="male";
+console.log(person)
+console.log(person.hobbies); 
+console.log(person["name"]);
