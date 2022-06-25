1e385f65e8f149c277350199c0b007779a64f097
irishavmishra <irishavmishra@proton.me>
2022-06-25 07:39:00 +0530

    My javascript learning journey

---
+++ 44functiondeclaration.js
@@ -0,0 +1,63 @@
+// // Function declaration
+
+function singHappyBirthday(){
+
+    console.log("Happy birthday To You......");
+
+}
+
+// // // singHappyBirthday();
+
+// // function sumTwoNumber(number1, number2){
+// // return number1 + number2;
+
+// // }
+
+// // const returnedValue = sumTwoNumber(5,7);
+// // console.log(returnedValue);
+
+// // function isEven(number) {
+// //   if (number % 2 === 0) {
+// //     return true;
+// //   } else {
+// //     return false;
+// //   }
+// // }
+
+// // function isEven(number) {
+// //   if (number % 2 === 0) {
+// //     return true;
+// //   }
+// //   return false;
+// // }
+
+// // function isEven(number) {
+// //   return number % 2 === 0;
+// // }
+
+// // console.log(isEven(7));
+
+// // function
+
+// // Input : String
+// // output : firstCharacter
+
+// // function firstCharacter(anystring){
+// //     return anystring[0];
+
+// // };
+
+// // console.log(firstCharacter("dbhj"));
+
+// function findTarget(array, target) {
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] === target) {
+//       return i;
+//     }
+//   }
+//   return -1;
+// }
+
+// const myArray =[1,3,5,15,68]
+// const ans = findTarget(myArray,5)
+// console.log(ans);
