73f15eeb88b3f6d3081a1fd77cd0b269571b33e2
irishavmishra <irishavmishra@proton.me>
2022-06-25 07:39:00 +0530

    My javascript learning journey

---
+++ 42objectsinsidearray.js
@@ -0,0 +1,17 @@
+// Objects inside Array
+
+
+const users = [
+    {userId:1, fullName:"Rishav Mishra", gender:"male",},
+    {userId:2, fullName:"fishav Mishra", gender:"male",},
+    {userId:3, fullName:"tishav Mishra", gender:"male",},
+    {userId:4, fullName:"dishav Mishra", gender:"male",},
+
+
+
+];
+for(let user of users){
+    // console.log(user)
+    console.log(user.fullName)
+};
+
