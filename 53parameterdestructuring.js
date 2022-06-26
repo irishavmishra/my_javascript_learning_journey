4ebf7b648095c3bac3ea19a0531b3c84a604d671
irishavmishra <irishavmishra@proton.me>
2022-06-26 07:39:00 +0530

    My javascript learning journey

---
+++ 53parameterdestructuring.js
@@ -0,0 +1,23 @@
+// Parameter Destructuring
+
+// Object
+
+
+
+const person = {
+    firstName: "harshit",
+    gender: "male"
+
+}
+
+// function printDetails(obj){
+//     console.log(obj.firstName);
+//     console.log(obj.gender);
+// }
+
+function printDetails({firstName,gender}){
+    console.log(firstName);
+    console.log(gender);
+}
+
+printDetails(person);
