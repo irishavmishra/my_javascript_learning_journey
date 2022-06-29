c70951bc72c3821ddc802d914141284b0403d957
irishavmishra <irishavmishra@proton.me>
2022-06-29 07:39:00 +0530

    My javascript learning journey

---
+++ 66iterablesarraylikeobjects.js
@@ -0,0 +1,26 @@
+// iterables array like object
+
+// iterables
+// jispe hum for of loop laga sakein
+// String, array are iterable
+
+// const firstName="ritik";
+// for(let name of firstName){
+
+//     console.log(name)
+// };
+
+// const items = ["item1", "item2","item3"];
+// for(let item of items){
+//     console.log(item);
+// }
+
+
+// Array like Object
+// jinke pas length property hoti hai
+// jinko hum index se access kar sakte he 
+// example:- String 
+
+const firstName = "ritik";
+console.log(firstName.length);
+console.log(firstName[3]);
