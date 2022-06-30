103f38ca79ff7dcee59f2520255e9ac289db501e
irishavmishra <irishavmishra@proton.me>
2022-06-30 07:39:00 +0530

    My javascript learning journey

---
+++ 79.js
@@ -0,0 +1,29 @@
+const userMethord = {
+    about: function(){
+        return `${this.firstName} is ${this.age} year old`
+    },
+    is18: function(){
+        return this .age >= 18;
+    }
+}
+
+
+function createUser(firstName, lastName, email, age,address,){
+    const user = Object.create(userMethord);
+    user.firstName = firstName;
+    user.lastName = lastName;
+    user.email = email;
+    user.age = age;
+    user.address = address;
+    return user;
+}
+
+const user1 = createUser("ritik", "mishra", "ritikmishra@gmail.com",23,"my address");
+const user2 = createUser("ritik2", "mishra2", "ritikmishra@gmail.com",23,"my address");
+const user3 = createUser("ritik3", "mishra3", "ritikmishra@gmail.com",23,"my address");
+
+
+
+console.log(user1)
+console.log(user1.about());
+// console.log(user1.is18());
