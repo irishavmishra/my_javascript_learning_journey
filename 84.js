637a859e393ca86e00bb1cb0a942aa9a7cd32dda
irishavmishra <irishavmishra@proton.me>
2022-07-01 07:39:00 +0530

    My javascript learning journey

---
+++ 84.js
@@ -0,0 +1,32 @@
+
+function CreateUser(firstName, lastName, email, age,address,){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.age = age;
+    this.address = address;
+}
+
+
+CreateUser.prototype.about = function(){
+    return `${this.firstName} is ${this.age} year old`
+};
+
+CreateUser.prototype.is18 = function(){
+    return this .age >= 18;
+};
+
+
+const user1 = new CreateUser("ritik", "mishra", "ritikmishra@gmail.com",23,"my address");
+const user2 = new CreateUser("ritik2", "mishra2", "ritikmishra@gmail.com",23,"my address");
+const user3 = new CreateUser("ritik3", "mishra3", "ritikmishra@gmail.com",23,"my address");
+
+
+for(let key in user1){
+    // console.log(key);
+    if(user1.hasOwnProperty(key)){
+        console.log(key);
+    }
+}
+
+
