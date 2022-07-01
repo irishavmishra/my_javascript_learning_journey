dfe312cab79d6dafe7a6d692a7a1386cabb67a0a
irishavmishra <irishavmishra@proton.me>
2022-07-01 07:39:00 +0530

    My javascript learning journey

---
+++ 89.js
@@ -0,0 +1,42 @@
+class Animal {
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+    eat(){
+        return `${this.name} is eating`;
+    }
+
+    isSuperCute(){
+        return this.age <= 1;
+    }
+
+    isCute(){
+        return true;
+    }
+}
+
+// const animal1 = new Animal("tom", 1);
+// console.log(animal1);
+// console.log(animal1.eat());
+// console.log(animal1.isCute());
+// console.log(animal1.isSuperCute());
+
+class Dog extends Animal{
+    constructor(name, age, speed){
+        super(name,age);
+        this.speed = speed;
+    }
+
+    eat(){
+        return "is eating new thing"
+    }
+
+    run(){
+        return `${this.name} is runing at ${this.speed}kmph`
+    }
+}
+
+const tommy = new Dog("tommy",3,45);
+console.log(tommy.run())
+console.log(tommy.eat())
