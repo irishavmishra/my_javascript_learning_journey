7f604db3c9fa722a7771a278298295e58afd1c13
irishavmishra <irishavmishra@proton.me>
2022-07-01 07:39:00 +0530

    My javascript learning journey

---
+++ 81.js
@@ -0,0 +1,32 @@
+function hello(){
+    console.log("hello world");
+}
+
+// const hello = {key1: "value1"}
+// const hello = ["value"]
+
+// javascript function ===> function + Object
+// console.log(hello.name);
+
+// you can add your own properties 
+// hello.myOwnProperty = "very unique value";
+// console.log(hello.myOwnProperty);
+
+// console.log(hello.prototype);
+
+// only function provide prototype property 
+
+
+// if(hello.prototype){
+//     console.log("prototype is present");
+
+// }else{
+//     console.log("prototype is not present")
+// }
+
+
+// hello.prototype.abc = "abc";
+// hello.prototype.sing = function(){
+//     return "lalala la"
+// }
+// console.log(hello.prototype.sing());
